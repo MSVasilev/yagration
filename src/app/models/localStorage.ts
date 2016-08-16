a2c0980a7268b0f1e3c1bb6707728af65a0f0530
Михаил <33misha@mail.ru>
2016-08-16 19:13:52 +0300

    av-grid, перегруппировка по 1С, сохранение состояния LS

---
+++ src/app/models/localStorage.ts
@@ -0,0 +1,21 @@
+function getRowListState() {
+    return JSON.parse(localStorage.getItem('rowListGroupExpandState'));
+}
+function setRowListState(o:string) {
+    localStorage.setItem('rowListGroupExpandState', JSON.stringify(o));
+    return false;
+}
+function deleteRowListState(o:string) {
+    localStorage.removeItem("rowListGroupExpandState");
+    return false;
+}
+
+
+
+function getLocalStorage() {
+    return JSON.parse(localStorage.getItem('ecarex.ru'));
+}
+function setLocalStorage(o:string) {
+    localStorage.setItem('ecarex.ru', JSON.stringify(o));
+    return false;
+}
